b78bb0eb1132d690317f3163f17a6b07b1cd66b8
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-04-29 09:59:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 3000;
 const cors = require("cors");
 
 const authRouter = require("./kakao-auth/kakao/kakao");
